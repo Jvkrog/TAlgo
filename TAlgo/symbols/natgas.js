8e496261f534ce7877ac5959aa45cd6d0241672b
Jvkrog <republicofgamerpro@gmail.com>
2026-03-15 15:16:16 +0530

    Add files via upload

---
+++ TAlgo/symbols/natgas.js
@@ -0,0 +1,7 @@
+module.exports = {
+    tradingsymbol:    "NATGASMINI26MARFUT",
+    exchange:         "MCX",
+    instrument_token: 121628679,
+    lotMultiplier:    1250,
+    maxLots:          4
+};
---
+++ TAlgo/symbols/zinc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    tradingsymbol:    "ZINC26MARFUT",
+    exchange:         "MCX",
+    instrument_token: 124841479,
+    lotMultiplier:    5000,
+    maxLots:          2
+};
