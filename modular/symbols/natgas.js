f4b5934608a5d95b233e867ed798717c29d7ead5
Jvkrog <republicofgamerpro@gmail.com>
2026-03-27 19:12:18 +0530

    .

--- modular/symbols/natgas.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    tradingsymbol:    "NATGASMINI26MARFUT",
-    exchange:         "MCX",
-    instrument_token: 121628679,
-    lotMultiplier:    1250,
-    maxLots:          4
-};
--- modular/symbols/zinc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    tradingsymbol:    "ZINC26MARFUT",
-    exchange:         "MCX",
-    instrument_token: 124841479,
-    lotMultiplier:    5000,
-    maxLots:          2
-};
